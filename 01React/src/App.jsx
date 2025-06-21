55cf74938dbc3a5ae11169ef7b6dcf42fd48ec3c
Sagar paul <80667455+Devilwalkers@users.noreply.github.com>
2025-06-21 06:36:03 +0000

    commit

--- 01React/src/App.jsx
+++ 01React/src/App.jsx
@@ -1,11 +1,14 @@
-
+import Dev from "./dev"
 
 function App() {
   return (
-    <>
+    // <></> Fragment
+    <> 
     <h1>Sagar paul</h1>
     <button>Vote me</button>
+    <Dev />
     </>
+    
   )
 }
 export default App
---
+++ 01React/src/dev.jsx
@@ -0,0 +1,7 @@
+function Dev(){
+    // Function should be start with Capital letter
+    return(
+        <h1>Voted already</h1>
+    )
+}
+export default Dev;
